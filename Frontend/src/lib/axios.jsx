f1c169409260fe1949412b78ce375efd7d45d097
Ashokpatel8090 <149434558+Ashokpatel8090@users.noreply.github.com>
2025-01-24 16:24:08 +0530

    Update axios.jsx
    Added backend route user render

--- Frontend/src/lib/axios.jsx
+++ Frontend/src/lib/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://backend-nma9.onrender.com/api",
   withCredentials: true,
 });
