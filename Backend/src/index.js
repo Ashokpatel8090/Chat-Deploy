181cb44bd18b7090c15cc5fcce55eb0ce599595f
Ashok Patel <ashokpatel881095@gmail.com>
2025-01-25 22:47:49 +0530

    Deployment commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,10 +15,11 @@ const PORT = process.env.PORT || 8000
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true,
-}
+app.use(cors(
+//     {
+//     origin:'http://localhost:5173',
+//     credentials:true,
+// }
 ))
 
 app.use("/api/auth", authRoutes);
